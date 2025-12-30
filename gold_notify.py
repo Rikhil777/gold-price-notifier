c9cf071de1d6d7c4805a895643ccabacd9a8a1b5
Kakani Rikhil <143270886+Rikhil777@users.noreply.github.com>
2025-12-30 23:01:40 +0530

    API_removed

--- gold_notify.py
+++ gold_notify.py
@@ -1,36 +1,43 @@
 import requests
+from bs4 import BeautifulSoup
 from datetime import datetime
 import os
 
-GOLD_API_KEY = os.getenv("goldapi-qsuhmsmjr6mocl-io")
-BOT_TOKEN = os.getenv("8237637090:AAFusvBLzNsvKL40b9aKPYY5egJekBoW1TU")
-CHAT_ID = os.getenv("1856690962")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
 
-def get_gold_price():
-    url = "https://www.goldapi.io/api/XAU/INR"
+def get_gold_price_per_mg():
+    url = "https://www.goodreturns.in/gold-rates/"
     headers = {
-        "x-access-token": GOLD_API_KEY,
-        "Content-Type": "application/json"
+        "User-Agent": "Mozilla/5.0"
     }
+
     response = requests.get(url, headers=headers, timeout=10)
-    data = response.json()
-    price_per_gram = data["price"] / 31.1035
-    return round(price_per_gram, 2)
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    # 24K gold price per gram
+    price_text = soup.find("td", class_="gold-rate-24k").text
+    price_per_gram = float(price_text.replace("₹", "").replace(",", "").strip())
+
+    # Convert gram → mg
+    price_per_mg = price_per_gram / 1000
+
+    return round(price_per_mg, 2)
 
 def send_telegram_message(message):
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
     payload = {"chat_id": CHAT_ID, "text": message}
     requests.post(url, data=payload, timeout=10)
 
-price = get_gold_price()
+price_mg = get_gold_price_per_mg()
 today = datetime.now().strftime("%d %b %Y")
 
 message = (
-    f"🟡 Gold Price Update\n\n"
+    f"🟡 Gold Locker Update\n\n"
     f"📅 Date: {today}\n"
-    f"💰 24K Gold Price:\n"
-    f"₹{price} per gram\n\n"
-    f"(Approx Google Pay Gold rate)"
+    f"💰 24K Gold Price\n"
+    f"₹{price_mg} per mg\n\n"
+    f"(Like Google Pay Gold Locker)"
 )
 
 send_telegram_message(message)
